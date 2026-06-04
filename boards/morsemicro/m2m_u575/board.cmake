3a2e934df01328f2a72b610a9a19d8fedaedc671
Craig Peacock <craigp@beyondlogic.org>
2026-06-04 21:16:23 +0930

    Remove boards to address:   Error message: ERROR: Malformed board YAML file:   /home/cpeacock/zephyrproject/modules/lib/morsemicro/boards/morsemicro/m2m_u575/board.yml

--- boards/morsemicro/m2m_u575/board.cmake
+++
@@ -1,8 +0,0 @@
-board_runner_args(openocd "--tcl-port=6666")
-board_runner_args(openocd --cmd-pre-init "gdb_report_data_abort enable")
-board_runner_args(openocd "--no-halt")
-
-board_runner_args(jlink "--device=STM32U575ZI" "--reset-after-load")
-
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
--- boards/morsemicro/mm6108_ekh05_v3/board.cmake
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024 STMicroelectronics
-# SPDX-License-Identifier: Apache-2.0
-
-board_runner_args(openocd "--tcl-port=6666")
-board_runner_args(openocd --cmd-pre-init "gdb_report_data_abort enable")
-board_runner_args(openocd "--no-halt")
-
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
